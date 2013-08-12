dde7394cc103880082a715ecc9caa4a186c3268f
inokappa <inokara@gmail.com>
2013-08-12 10:45:26 +0900

    add static args version

--- get-mysql-info.rb
+++ get-mysql-info.rb
@@ -18,52 +18,74 @@ class Con
   end
 end
 
-# 
-class Mycommand < Con
-  def mysql_variables_com(myuser,mypass,*args)
-    my_rets = []
-    args.each do |params|
-     params.each do |param|
-      if !(param == "variables")
-        exec = ssh_exec("mysql -u#{myuser} -p#{mypass} --execute='show variables' | egrep '^#{param}' | sed 's/#{param}//g'")
-        if exec == nil
-          my_rets << ["#{param}" => "N/A"]
-        else
-          my_rets << ["#{param}" => "#{exec}".strip!]
-        end
-      end
-     end
-     return my_rets 
-    end
+class Mysqlinfo < Con
+  def get_os_name
+    os_raw = ssh_exec("uname -s")
+    os = os_raw.strip
   end
-  def server_status
-    os_rets = []
+  def get_os_mem
     os_raw = ssh_exec("uname -s")
     if os_raw == "FreeBSD\n"
       mem_raw = ssh_exec("grep memory /var/run/dmesg.boot | egrep 'real|usable' | awk '{print $5,$6}'")
     else
       mem_raw = ssh_exec("cat /proc/meminfo  | grep MemTotal | awk '{print $2,$3}'")
     end
-    os_rets << ["OS" => "#{os_raw}".strip!]
-    os_rets << ["Mem" => "#{mem_raw}".strip!]
+    mem = mem_raw.strip
+  end
+  def get_wait_timeout(myuser,mypass)
+    wait_timeout_raw = ssh_exec("mysql -u#{myuser} -p#{mypass} --execute='show variables' | egrep '^wait_timeout' | sed s/wait_timeout//g")
+    wait_timeout = wait_timeout_raw.strip
+  end
+  def get_thread_cache_size(myuser,mypass)
+    thread_cache_size_raw = ssh_exec("mysql -u#{myuser} -p#{mypass} --execute='show variables' | egrep '^thread_cache_size' | sed s/thread_cache_size//g")
+    thread_cache_size = thread_cache_size_raw.strip
+  end
+  def get_max_connection(myuser,mypass)
+    max_connection_raw = ssh_exec("mysql -u#{myuser} -p#{mypass} --execute='show variables' | egrep '^max_connections' | sed s/max_connections//g")
+    max_connection = max_connection_raw.strip
+  end
+  def get_innodb_buffer_pool_size(myuser,mypass)
+    innodb_buffer_pool_size_raw = ssh_exec("mysql -u#{myuser} -p#{mypass} --execute='show variables' | egrep '^innodb_buffer_pool_size'| sed s/innodb_buffer_pool_size//g")
+    if innodb_buffer_pool_size_raw == nil
+      innodb_buffer_pool_size = "N/A"
+    else
+      innodb_buffer_pool_size = innodb_buffer_pool_size_raw.strip
+    end
+  end
+  def get_mysql_version(myuser,mypass)
+    version_raw = ssh_exec("mysql -u#{myuser} -p#{mypass} --execute='show variables' | egrep '^version'| grep -v 'version_' | sed s/version//g")
+    version = version_raw.strip
   end
 end
 #
-filename = './server_info'
+rows = []
+filename = "./server_info"
 file = open(filename)
-result = []
 while text = file.gets do
-  arr = text.split(",")
-  chk = Mycommand.new("#{arr[0]}","#{arr[1]}","#{arr[2]}")
+  raw = text
+  arr = raw.split(",")
+  chk = Mysqlinfo.new("#{arr[0]}","#{arr[1]}","#{arr[2]}")
   myuser = "#{arr[3]}"
   mypass = "#{arr[4]}"
-  my_rets = chk.mysql_variables_com(myuser,mypass,ARGV)
-  os_rets = chk.server_status
-  result << "#{arr[0]}"
-  result << os_rets
-  result << my_rets
-  puts result
-  #result.to_json(:root => false)
-  #json = JSON.pretty_generate([result])
-  #puts json
+  rows << [\
+    "#{arr[0]}",\
+    "#{chk.get_os_name}",\
+    "#{chk.get_os_mem}",\
+    "#{chk.get_mysql_version(myuser,mypass)}",\
+    "#{chk.get_wait_timeout(myuser,mypass)}",\
+    "#{chk.get_thread_cache_size(myuser,mypass)}",\
+    "#{chk.get_max_connection(myuser,mypass)}",\
+    "#{chk.get_innodb_buffer_pool_size(myuser,mypass)}"]
+
+  table = Terminal::Table.new :headings => [\
+    'Host',\
+    'OS',\
+    'Memory',\
+    'MySQL version',\
+    'wait_timeout',\
+    'thread_cache_size',
+    'max_connection',
+    'innodb_buffer_pool_size'], :rows => rows
 end
+#
+puts table
